9c7169de602002f52cad26980fc942b9dceaac10
Jonathan W <jwilliams12117@gmail.com>
2022-05-13 15:36:10 -0700

    Added resolvers for user and thought data

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,8 +1,30 @@
+const {User, Thought} = require("../models");
+
+// The resolvers handle the queries for any of the particular "endpoints," like a REST API would
+// with routers.  The objects nested in the Query object are all Mongoose code.
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return "Hello World!";
-        }
+        thoughts: async () => {
+            return Thought.find().sort({createdAt: -1});
+        },
+        thought: async (parent, {_id}) => {
+            return Thought.findOne({_id})
+        },
+        // Get all users
+        users: async () => {
+            return User.find()
+                .select("-__v -password")
+                .populate("friends")
+                .populate("thoughts")
+        },
+        // Get a single user by username
+        user: async(parent, {username}) => {
+            return User.findOne({username})
+                .select("-__v -password")
+                .populate("friends")
+                .populate("thoughts")
+        },
     }
 }
 
--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -2,8 +2,34 @@ const {gql} = require("apollo-server-express");
 
 // Create typeDefs
 const typeDefs = gql`
+
+    type Thought {
+        _id: ID
+        thoughtText: String
+        createdAt: String
+        username: String
+        reactionCount: Int
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        friendCount: Int
+        thoughts: [Thought]
+        friends: [User]
+    }
+
     type Query {
-        helloWorld: String
-    }`;
+        users: [User]
+        user(username: String!): User
+        thoughts(username: String): [Thought]
+        thought(_id: ID!): Thought
+    }
+`
+
+// user(username: String!): User
+// is a query that can accept an optional arg username, and will then return that particular user.
+// It can be left blank as long as the exclamation point is missing, otherwise this is required.
 
 module.exports = typeDefs;
